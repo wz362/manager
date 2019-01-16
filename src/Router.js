97e5ae710f1178732e6ec77df6a9b7ab013d7614
wenjiazhang <kathywj.zhang@hotmail.com>
2019-01-16 09:22:55 -0500

    Employee Create Form

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
     return (
@@ -12,11 +13,16 @@ const RouterComponent = () => {
                 </Scene>
                 <Scene key="main">
                     <Scene 
-                        rightTitle="add"
-                        onRight={() => { console.log('Right') }}
+                        onRight={() => Actions.employeeCreate()}
+                        rightTitle="Add"                   
                         key="employeeList" 
                         component={EmployeeList} 
                         title="Employees" 
+                    />
+                    <Scene 
+                        key="employeeCreate"
+                        component={EmployeeCreate}
+                        title="Create Employee"
                     />
                 </Scene>
             </Scene>
