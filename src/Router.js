71915d13f835fad2e4fee16c347d28e2d52b54c8
wenjiazhang <kathywj.zhang@hotmail.com>
2019-01-16 00:15:21 -0500

    Navigation between routes

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
     return (
         <Router>
-            <Scene key="root">
-                <Scene key="login" component={LoginForm} title="Please Login" />
+            <Scene key="root" hideNavBar>
+                <Scene key="auth">
+                    <Scene key="login" component={LoginForm} title="Please Login" initial />
+                </Scene>
+                <Scene key="main">
+                    <Scene 
+                        rightTitle="add"
+                        onRight={() => { console.log('Right') }}
+                        key="employeeList" 
+                        component={EmployeeList} 
+                        title="Employees" 
+                    />
+                </Scene>
             </Scene>
         </Router>
     );
