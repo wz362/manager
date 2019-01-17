13755d99a039b480cfec0c394134bcbb48ea45eb
wenjiazhang <kathywj.zhang@hotmail.com>
2019-01-17 01:07:32 -0500

    Edit Employee

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,14 +1,21 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { employeeUpdate, employeeCreate } from '../actions';
+import { employeeUpdate, employeeSave } from '../actions';
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './EmployeeForm';
 
-class EmployeeCreate extends Component {
+class EmployeeEdit extends Component {
+    componentWillMount() {
+        _.each(this.props.employee, (value, prop) => {
+            this.props.employeeUpdate({ prop, value });
+        });
+    }
+
     onButtonPress() {
         const { name, phone, shift } = this.props;
 
-        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+        this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
     }
 
     render() {
@@ -17,20 +24,13 @@ class EmployeeCreate extends Component {
                 <EmployeeForm {...this.props} />
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>
-                        Create
+                        Save Changes
                     </Button>
                 </CardSection>
             </Card>
         );
     }
 }
-
-const styles = {
-    pickerTextStyle: {
-        fontSize: 18,
-        paddingLeft: 20
-    }
-};
 
 const mapStateToProps = (state) => {
     const { name, phone, shift } = state.employeeForm;
@@ -39,5 +39,5 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, { 
-    employeeUpdate, employeeCreate 
-})(EmployeeCreate);
+    employeeUpdate, employeeSave
+})(EmployeeEdit);
